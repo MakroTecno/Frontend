3f02841929b9ee9b227250a64721285e66591262
MakroTecno <victorcalvache1@gmail.com>
2020-05-18 23:09:28 -0500

    Modificacion del index y arreglos en estilos y scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-function status()
-{
-	alert('hola');
+function myFunction() {
+    var menu_vertical = document.getElementById("menu_vertical_login");
+    var panel = document.getElementById("panel_general");
+    if (menu_vertical.style.display === "none" && panel.style.display === "none") {
+        menu_vertical.style.display = "block";
+        panel.style.display = "block";
+    } else {
+        menu_vertical.style.display = "none";
+        panel.style.display = "none";
+    }
 }
